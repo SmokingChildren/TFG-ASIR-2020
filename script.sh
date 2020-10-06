ead336e7b6b171b4386444294c22218532256649
SmokingChild <50420257+SmokingChildren@users.noreply.github.com>
2020-10-06 20:20:17 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+#Autor: Juan Carlos Collado Custodio
+#Módulo: Proyecto de Fin de Grado
+#Curso: ASIR 2019-2020
+#Título: Shell script con interfaz gráfica para gestión de usuarios, procesos y servicios
+
+clear
