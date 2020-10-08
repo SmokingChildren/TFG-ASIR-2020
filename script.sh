b8e062f75af35d0f47acf505a4e7d776eba0c6fb
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-08 20:46:40 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,3 +5,11 @@
 #Título: Shell script con interfaz gráfica para gestión de usuarios, procesos y servicios
 
 clear
+
+while [[ true ]]; do
+echo "menu"
+case "$selection" in
+1) #Gestión de usuarios
+2) #Gestión de procesos
+3) #Gestión de servicios
+4) #Actualización del sistema
