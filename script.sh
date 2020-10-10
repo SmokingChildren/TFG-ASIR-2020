4a59b28b38db76e8ca0d793a41702107e45884e0
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-10 15:46:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,70 @@
 #!/bin/bash
+#==========================================================================================
 #Autor: Juan Carlos Collado Custodio
 #Módulo: Proyecto de Fin de Grado
 #Curso: ASIR 2019-2020
 #Título: Shell script con interfaz gráfica para gestión de usuarios, procesos y servicios
+#==========================================================================================
+
+#1) Gestión de usuarios
+    #a) Crear usuario
+    #b) Buscar usuario
+        #b.1) Modificar datos del usuario
+        #b.2) Borrar usuario
+    #c) Consultar último usuario creado
+
+#2) Gestión de procesos
+    #a) Consultar procesos activos en este momento
+    #b) Consultar cantidad de memoria libre
+    #c) Consultar cuánto tiempo lleva el servidor encendido
+    #d) Consultar quién está logeado en el sistema
+    #e) Consultar los 10 procesos que más memoria están consumiendo
+
+#3) Gestión de servicios
+    #a) Mostrar servicios activos en este momento
+    #b) Mostrar tiempo que ha tardado el equipo en arrancar
+    #c) Mostrar cuánto han tardado los procesos en arrancar
+    #d)
+    #e)
+
+#4) Información del sistema
+    #a)
+    #b)
+    #c)
+    #d)
+    #e)
+
+#q) Salir del menú
+
+#==========================================================================================
+
+# VARIABLES
+
+#==========================================================================================
+
+# FUNCIONES
+
+#==========================================================================================
+
+# Inicio del script
 
 clear
 
 while [[ true ]]; do
-echo "menu"
-case "$selection" in
-1) #Gestión de usuarios
-2) #Gestión de procesos
-3) #Gestión de servicios
-4) #Actualización del sistema
+    echo "menu"
+    case "$selection" in
+    a)
+        echo "TO DO"
+        ;;
+    b)
+        echo "TO DO"
+        ;;
+    c)
+        echo "TO DO"
+        ;;
+    q)
+        echo "Saliendo"
+        break
+        ;;
+    esac
+done
