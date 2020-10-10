8f18ccbf625bbc36c299b6c1cb83440d5d6390e9
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-10 16:27:11 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,12 +6,17 @@
 #Título: Shell script con interfaz gráfica para gestión de usuarios, procesos y servicios
 #==========================================================================================
 
-#1) Gestión de usuarios
+#1) Gestión de usuarios y grupos
     #a) Crear usuario
     #b) Buscar usuario
         #b.1) Modificar datos del usuario
         #b.2) Borrar usuario
     #c) Consultar último usuario creado
+    #d) Crear grupo
+    #e) Buscar grupo
+        #e.1) Modificar datos del grupo
+        #e.2) Añadir o quitar miembros
+        #e.3) Borrar grupo
 
 #2) Gestión de procesos
     #a) Consultar procesos activos en este momento
@@ -24,15 +29,11 @@
     #a) Mostrar servicios activos en este momento
     #b) Mostrar tiempo que ha tardado el equipo en arrancar
     #c) Mostrar cuánto han tardado los procesos en arrancar
-    #d)
-    #e)
+    #d) Mostrar servicios disponibles y seleccionar
+        #d.1) Arrancar o detener servicio seleccionado (solo 1, opción tipo radio)
 
 #4) Información del sistema
-    #a)
-    #b)
-    #c)
-    #d)
-    #e)
+    #a) 
 
 #q) Salir del menú
 
@@ -44,6 +45,18 @@
 
 # FUNCIONES
 
+function menu() {
+    clear
+    echo
+    echo -e "\t\t\tAdmin Menu\n"
+    echo -e "\t1. Display Total Disk Space"
+    echo -e "\t2. Display All Logged in Users"
+    echo -e "\t3. Display Memory Usage"
+    echo -e "\t0. Exit Menu\n\n"
+    echo -en "\t\tEnter an Option: "
+    read -n 1 selection
+}
+
 #==========================================================================================
 
 # Inicio del script
@@ -51,8 +64,8 @@
 clear
 
 while [[ true ]]; do
-    echo "menu"
-    case "$selection" in
+    menu
+    case $selection in
     a)
         echo "TO DO"
         ;;
@@ -65,6 +78,9 @@ while [[ true ]]; do
     q)
         echo "Saliendo"
         break
+        ;;
+    *)
+        echo "Opción inválida."
         ;;
     esac
 done
