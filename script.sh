f43e800c3e7d28ad5d9811293b88cc0fc4eb55c0
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-14 21:28:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,9 +14,9 @@
 
 #Para lanzar el resultado de un comando: $ whiptail --textbox /dev/stdin 40 80 <<<"$(ls -l)"
 
-function menu() {
+function mainmenu() {
     clear
-    option=$(whiptail --title "Administración del Sistema" --menu "Elige una opción" 15 60 4 \
+    mainmenu_option=$(whiptail --title "Administración del Sistema" --menu "Elige una opción" 15 60 4 \
         "1" "Gestión de usuarios" \
         "2" "Gestión de procesos" \
         "3" "Gestión de servicios" \
@@ -25,15 +25,43 @@ function menu() {
     echo $?
 }
 
+function user_mgt_menu() {
+    usermenu_option=$(whiptail --title "Gestión de usuarios" --menu "Seleccione una opción" 10 60 4 \
+    "1" "Añadir nuevo usuario." \
+    "2" "Modificar datos de usuario." \
+    "3" "Eliminar usuario del sistema." \
+    "4" "Ver información de usuario." \
+    "0" "Volver" 3>&1 1>&2 2>&3)
+}
+
 #==========================================================================================
 
 # Inicio del script
 
 while [[ true ]]; do
-    menu
-    case $option in
+    mainmenu
+    case $mainmenu_option in
     1)
-        whiptail --title "Mensaje" --msgbox "Opción 1" 40 80
+        #whiptail --title "Mensaje" --msgbox "Opción 1" 40 80
+        while [[ true ]]; do
+            user_mgt_menu
+            case $usermenu_option in
+            1) whiptail --title "Mensaje" --msgbox "Añadir usuario" 40 80
+                ;;
+            2)
+                whiptail --title "Mensaje" --msgbox "Modificar usuario" 40 80
+                ;;
+            3)
+                whiptail --title "Mensaje" --msgbox "Eliminar usuario" 40 80
+                ;;
+            4)
+                whiptail --title "Mensaje" --msgbox "Información del usuario." 40 80
+                ;;
+            0)
+                break 
+                ;;
+            esac
+        done
         ;;
     2)
         whiptail --title "Mensaje" --msgbox "Opción 2" 40 80
