9a47713b45a5cdad954b3696c168005e1e2428ae
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-16 17:57:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@
 
 function mainmenu() {
     clear
-    mainmenu_option=$(whiptail --title "Administración del Sistema" --menu "Elige una opción" 15 60 4 \
+    mainmenu_option=$(whiptail --title "Administración del Sistema" --menu "Elige una opción" 25 60 4 \
         "1" "Gestión de usuarios" \
         "2" "Gestión de procesos" \
         "3" "Gestión de servicios" \
@@ -24,7 +24,7 @@ function mainmenu() {
 }
 
 function user_mgt_menu() {
-    usermenu_option=$(whiptail --title "Gestión de usuarios" --menu "Seleccione una opción" 15 60 5 \
+    usermenu_option=$(whiptail --title "Gestión de usuarios" --menu "Seleccione una opción" 15 50 5 \
     "1" "Añadir nuevo usuario." \
     "2" "Modificar datos de usuario." \
     "3" "Eliminar usuario del sistema." \
@@ -33,16 +33,16 @@ function user_mgt_menu() {
 }
 
 function process_mgt_menu(){
-    processmenu_option=$(whiptail --title "Gestión de procesos" --menu "Seleccione una opción" 15 60 5 \
-    "1" "Procesos activos." \
-    "2" "Procesos que más consumen en este momento (max. 10)." \
-    "3" "Detener procesos (submenú)." \
+    processmenu_option=$(whiptail --title "Gestión de procesos" --menu "Seleccione una opción" 15 50 5 \
+    "1" "Ver procesos activos." \
+    "2" "Ver procesos que más consumen en este momento (max. 10)." \
+    "3" "Detener o arrancar procesos." \
     "4" "Opcion no implementada." \
     "0" "Volver" 3>&1 1>&2 2>&3)
 }
 
 function services_mgt_menu(){
-    servicesmenu_option=$(whiptail --title "Gestión de servicios" --menu "Seleccione una opción" 15 60 5 \
+    servicesmenu_option=$(whiptail --title "Gestión de servicios" --menu "Seleccione una opción" 15 50 5 \
     "1" "Información del sistema." \
     "2" "Memoria en uso y memoria disponible." \
     "3" "Tiempo que lleva el servidor en marcha." \
