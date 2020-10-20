a9c96abbfac8b504f0c6faffbfdca188a5e1b50e
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-20 19:07:27 +0200

    Añadido test.sh para pruebas

--- test.sh
+++ test.sh
@@ -13,3 +13,8 @@
 # FUNCIONES
 
 #Para lanzar el resultado de un comando: $ whiptail --textbox /dev/stdin 40 80 <<<"$(ls -l)"
+
+nombre=$(whiptail --inputbox "Introduce texto" 8 39 Nombre --title "Ejemplo" 3>&1 1>&2 2>&3)
+password=$(whiptail --passwordbox "Introduce password" 8 39 --title "Password" 3>&1 1>&2 2>&3)
+echo "El nombre es " $nombre
+echo "La contraseña es " $password
