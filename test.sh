3a3514d3084a9dead2098cab210e82f539b7784a
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-24 17:08:31 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -13,30 +13,30 @@ while [[ true ]]; do
     color_menu
     case $colormenu_opt in
     1)
-        export NEWT_COLORS='
+        NEWT_COLORS='
         window=,red
         border=white,red
         textbox=white,red
         button=black,white
-        ' \
+        '
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     2)
-        export NEWT_COLORS='
+        NEWT_COLORS='
         window=,blue
         border=white,blue
         textbox=white,blue
         button=black,white
-        ' \
+        '
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     3)
-        export NEWT_COLORS='
+        NEWT_COLORS='
         window=,green
         border=white,green
         textbox=white,green
         button=black,white
-        ' \
+        '
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     0)
