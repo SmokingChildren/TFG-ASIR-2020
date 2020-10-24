b7335783eed44c8d63e0d4506ad413874292cdc9
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-24 12:43:03 +0200

    Ver comentarios
    Añadido arranque con sudo
    Comentadas las opciones antes de añadirlas. Muchos comentarios se han movido de línea simplemente.
    Opción 3.4 cambiada de df -h a parted -l por ser más "user-friendly".
    Ocultados botones de cancelar de los menús, no funcionaban con el bucle.

--- script.sh
+++ script.sh
@@ -12,11 +12,14 @@
 
 # FUNCIONES
 
-#Para lanzar el resultado de un comando: $ whiptail --textbox /dev/stdin 40 80 <<<"$(ls -l)"
+#Para lanzar el resultado de un comando (p. ej. "ls -l"): $ whiptail --textbox /dev/stdin 40 80 <<<"$(ls -l)"
+
+#Si el script no se ha lanzado con sudo, lo inicia ahora para poder modificar usuarios.
+[ $UID != 0 ] && exec sudo $0 "$@" 
 
 function mainmenu() { #De este menú derivan el resto de submenús.
     clear
-    mainmenu_option=$(whiptail --title "Administración del Sistema" --menu "Elige una opción" 25 60 4 \
+    mainmenu_option=$(whiptail --title "Administración del Sistema" --nocancel --menu "Elige una opción" 25 60 4 \
         "1" "Gestión de usuarios" \
         "2" "Gestión de procesos" \
         "3" "Gestión de servicios" \
@@ -24,16 +27,16 @@ function mainmenu() { #De este menú derivan el resto de submenús.
 }
 
 function user_mgt_menu() {
-    usermenu_option=$(whiptail --title "Gestión de usuarios" --menu "Seleccione una opción" 15 50 5 \
-    "1" "Añadir nuevo usuario." \
-    "2" "Modificar datos de usuario." \
-    "3" "Eliminar usuario del sistema." \
-    "4" "Ver información de usuario." \
+    usermenu_option=$(whiptail --title "Gestión de usuarios" --nocancel --menu "Seleccione una opción" 15 50 5 \
+    "1" "Añadir un nuevo usuario." \
+    "2" "Modificar datos de un usuario." \
+    "3" "Eliminar un usuario del sistema." \
+    "4" "Ver información del usuario actual." \
     "0" "Volver" 3>&1 1>&2 2>&3)
 }
 
 function process_mgt_menu(){
-    processmenu_option=$(whiptail --title "Gestión de procesos" --menu "Seleccione una opción" 15 65 5 \
+    processmenu_option=$(whiptail --title "Gestión de procesos" --nocancel --menu "Seleccione una opción" 15 65 5 \
     "1" "Ver procesos activos." \
     "2" "Ver procesos que más consumen en este momento (max. 10)." \
     "3" "Detener o arrancar procesos." \
@@ -42,7 +45,7 @@ function process_mgt_menu(){
 }
 
 function services_mgt_menu(){
-    servicesmenu_option=$(whiptail --title "Gestión de servicios" --menu "Seleccione una opción" 15 65 5 \
+    servicesmenu_option=$(whiptail --title "Gestión de servicios" --nocancel --menu "Seleccione una opción" 15 65 5 \
     "1" "Información del sistema." \
     "2" "Memoria en uso y memoria disponible." \
     "3" "Tiempo que lleva el servidor en marcha." \
@@ -58,31 +61,41 @@ function password_ask(){
 
 # Inicio del script
 
-while [[ true ]]; do #Se puede hacer con condición expresamente, pero no es necesario.
+while [[ true ]]; do
     mainmenu
     case $mainmenu_option in
     1)
         while [[ true ]]; do
             user_mgt_menu #Gestión de usuarios. Ver funciones.
             case $usermenu_option in
-            1) #whiptail --title "Mensaje" --msgbox "Añadir usuario" 40 80
+            1)
                 nombre=$(whiptail --inputbox "Introduce el nombre de usuario" 8 39 Nombre --title "Ejemplo" 3>&1 1>&2 2>&3)
                 password_ask
                 until [[ $password == $passwordcheck ]]; do
                     whiptail --title "Error" --msgbox "Las contraseñas no coinciden" 8 50
                     password_ask
-                done
-                useradd -m -p $(echo $password | openssl passwd -1 -stdin) $nombre #¿Añadir sudo o lanzar script como sudo?
-                whiptail --title "Mensaje" --msgbox "Usuario $nombre registrado correctamente." 40 80 #TO-DO: Condicional comprobando que no falla.
+                done                
+                useradd -m -p $(echo $password | openssl passwd -1 -stdin) $nombre
+                whiptail --title "Mensaje" --msgbox "Usuario $nombre registrado correctamente." 40 80 
+                #TO-DO: Condicional comprobando que no falla.
+                #Comprobar la variable $nombre si coincide con la última línea de /etc/passwd
                 ;;
             2)
-                whiptail --title "Mensaje" --msgbox "Modificar usuario" 40 80 #Estos mensajes hay que sustituirlos por las funciones correspondientes.
+                whiptail --title "Mensaje" --msgbox "Modificar datos de un usuario" 40 80
+                #Pedir nombre de usuario
+                #Comprobar que el usuario existe. Si no, salir al menú anterior.
+                #Mostrar menú radio con las opciones a cambiar. 
                 ;;
             3)
-                whiptail --title "Mensaje" --msgbox "Eliminar usuario" 40 80 #TO-DO: Añadir confirmación de eliminación.
+                whiptail --title "Mensaje" --msgbox "Eliminar usuario" 40 80
+                #Pedir nombre del usuario
+                #Comprobar que existe ese usuario
+                #Eliminar usuario y su directorio en /home
+                #Mensaje de confirmación
                 ;;
             4)
-                whiptail --title "Mensaje" --msgbox "Información del usuario." 40 80 #Grupos, carpetas y algo más de info (TBD).
+                whiptail --title "Mensaje" --msgbox "Información del usuario." 40 80 
+                #Grupos, carpetas y algo más de info (TBD).
                 ;;
             0)
                 break 
@@ -94,17 +107,21 @@ while [[ true ]]; do #Se puede hacer con condición expresamente, pero no es nec
         while [[ true ]]; do
             process_mgt_menu #Gestión de procesos. Ver funciones.
             case $processmenu_option in
-            1) whiptail --title "Mensaje" --msgbox "Procesos activos" 40 80 #TO-DO: ¿Mostrar resultado de un ps o un top?
+            1) 
+                whiptail --title "Mensaje" --msgbox "Procesos activos" 40 80 
+                #TO-DO: ¿Mostrar resultado de un ps o un top?
                 ;;
             2)
                 #Procesos que más consumen en este momento (max. 10)
                 whiptail --textbox /dev/stdin 20 40 <<<"$(ps -eo %mem,%cpu,comm --sort=-%mem | head -n 11)"
                 ;;
             3)
-                whiptail --title "Mensaje" --msgbox "Detener procesos (submenú)." 40 80 #TO-DO: Bucle for, mostrar procesos en opciones radio, detener proceso seleccionado.
+                whiptail --title "Mensaje" --msgbox "Detener procesos (submenú)." 40 80 
+                #TO-DO: Bucle for, mostrar procesos en opciones radio, detener proceso seleccionado.
                 ;;
             4)
-                whiptail --title "Mensaje" --msgbox "Opcion no implementada (Placeholder)." 40 80 #TBD. 
+                whiptail --title "Mensaje" --msgbox "Opcion no implementada (Placeholder)." 40 80 
+                #TBD. 
                 ;;
             0)
                 break 
@@ -116,7 +133,8 @@ while [[ true ]]; do #Se puede hacer con condición expresamente, pero no es nec
         while [[ true ]]; do
             services_mgt_menu #Gestión de servicios y sistema
             case $servicesmenu_option in
-            1) #Información del sistema.
+            1) 
+                #Información del sistema.
                 whiptail --textbox /dev/stdin 20 60 <<<"$(hostnamectl)"
                 ;;
             2)
@@ -129,7 +147,7 @@ while [[ true ]]; do #Se puede hacer con condición expresamente, pero no es nec
                 ;;
             4)
                 #Distribución de discos duros / particiones y su ocupación
-                whiptail --textbox /dev/stdin  30 90 <<<"$(df -h)"
+                whiptail --textbox /dev/stdin  30 90 <<<"$(parted -l)"
                 ;;
             0)
                 break 
