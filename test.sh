2a13cbaef1c8b883a8e6c832074edeb572f557e3
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-10-24 16:53:40 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,43 +1,41 @@
 #!/bin/sh
 
 color_menu() {
-    colormenu=$(
-        whiptail --title "Administración del Sistema" --nocancel --radiolist "Elige una opción" 25 60 4 \
+    colormenu=$(whiptail --title "Administración del Sistema" --nocancel --radiolist "Elige una opción" 25 60 4 \
         "1" "Color 1" \
         "2" "Color 2" \
         "3" "Color 3" \
         "0" "Salir" 3>&1 1>&2 2>&3)
 }
 
-while [[ true ]]; 
-do
+while [[ true ]]; do
     color_menu
     case $colormenu in
     1)
-    export NEWT_COLORS='
-    window=,red
-    border=white,red
-    textbox=white,red
-    button=black,white
-    ' \
+        export NEWT_COLORS='
+        window=,red
+        border=white,red
+        textbox=white,red
+        button=black,white
+        ' \
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     2)
-    export NEWT_COLORS='
-    window=,blue
-    border=white,blue
-    textbox=white,blue
-    button=black,white
-    ' \
+        export NEWT_COLORS='
+        window=,blue
+        border=white,blue
+        textbox=white,blue
+        button=black,white
+        ' \
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     3)
-    export NEWT_COLORS='
-    window=,green
-    border=white,green
-    textbox=white,green
-    button=black,white
-    ' \
+        export NEWT_COLORS='
+        window=,green
+        border=white,green
+        textbox=white,green
+        button=black,white
+        ' \
         whiptail --msgbox "Muestra de colores" 0 0
         ;;
     0)
