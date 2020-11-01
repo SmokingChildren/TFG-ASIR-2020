a4c2402e23d3625ca4edc23ea73f25e17b524fc1
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-11-01 12:45:10 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,54 +1,13 @@
-#!/bin/bash
-
-colormenuoption=""
-
-color_change_menu() {
-    clear
-    colormenuoption=$(
-        whiptail --title "Colores" --radiolist \
-        "Elige un color" 20 78 4 \
-        "1" "Rojo" ON \
-        "2" "Azul" OFF \
-        "3" "Verde" OFF \
-        "0" "Salir" OFF 3>&1 1>&2 2>&3
-    )
-}
-
-while [ true ]; do
-    color_change_menu
-    case $colormenuoption in
-    1)
-        export NEWT_COLORS='
-        window=,red
-        border=white,red
-        textbox=white,red
-        button=black,white
-        '
-        whiptail --msgbox "Muestra de colores" 0 0
-        ;;
-    2)
-        export NEWT_COLORS='
-        window=,blue
-        border=white,blue
-        textbox=white,blue
-        button=black,white
-        '
-        whiptail --msgbox "Muestra de colores" 0 0
-        ;;
-    3)
-        export NEWT_COLORS='
-        window=,green
-        border=white,green
-        textbox=white,green
-        button=black,white
-        '
-        whiptail --msgbox "Muestra de colores" 0 0
-        ;;
-    0)
-        exit
-        ;;
-    *)
-        whiptail --msgbox "Error" 0 0
-        ;;
-    esac
-done
+admincheck=$(
+    whiptail --title "Se requiere permiso de Administrador" --yesno
+    "Este script requiere permisos de administrador para funcionar correctamente. \n
+    ¿Eres administrador del equipo? S/N \n
+    (Utiliza TAB para cambiar de opción)" 10 10
+)
+if [[ admincheck = 0 ]]; then
+    #Si el script no se ha lanzado como sudoer, lo hace ahora.
+    [ $UID != 0 ] && exec sudo $0 "$@"
+    whiptail --title "Aceptado" --msgbox "Permiso de administrador aceptado. Puede continuar." 5 10
+else
+    whiptail --title "Aceptado" --msgbox "Permiso de administrador aceptado. Puede continuar." 5 10
+fi
