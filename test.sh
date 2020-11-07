76390bcbb70954b8b167f5430a4c9ed880116594
SmokingChildren <50420257+SmokingChildren@users.noreply.github.com>
2020-11-07 21:52:20 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ IFS=$'\n'
 usuarios=$(cat /etc/passwd | cut -d: -f3)
 for i in $usuarios; do
     usuario=$i
-    username=$(grep $usuario /etc/passwd | cut -d: -f1)
+    echo $i
+    username=$(head $usuario /etc/passwd | cut -d: -f1)
     echo $username
 done
-IFS=$OLDIFS
